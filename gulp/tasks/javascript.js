a6d37b3eef048dfd89581bf9fa184e034d297e63
Kalin Maldzhanski <djodjo@djodjo.org>
2016-02-27 20:16:09 +0100

    update to foundation6.2 and babel

--- gulp/tasks/javascript.js
+++ gulp/tasks/javascript.js
@@ -1,23 +1,25 @@
-var $             = require('gulp-load-plugins')();
-var browserSync   = require('browser-sync');
-var config        = require('../util/loadConfig').javascript;
-var gulp          = require('gulp');
-var isProduction  = require('../util/isProduction');
+var $ = require('gulp-load-plugins')();
+var browserSync = require('browser-sync');
+var config = require('../util/loadConfig').javascript;
+var gulp = require('gulp');
+var babel = require('gulp-babel');
+var isProduction = require('../util/isProduction');
 
-gulp.task('javascript', function() {
-  browserSync.notify(config.notification);
-  var uglify = $.if(isProduction, $.uglify()
-    .on('error', function (e) {
-      console.log(e);
-    }));
+gulp.task('javascript', function () {
+    browserSync.notify(config.notification);
+    var uglify = $.if(isProduction, $.uglify()
+        .on('error', function (e) {
+            console.log(e);
+        }));
 
-  return gulp.src(config.src)
-    .pipe($.sourcemaps.init())
-    .pipe($.concat(config.filename))
-    .pipe($.if(isProduction, $.uglify({ mangle: false })))
-    .pipe($.if(!isProduction, $.sourcemaps.write()))
-    // for live injecting (for production builds we write the revised version)
-    .pipe($.if(!isProduction, gulp.dest(config.dev.dest)))
-    // for future jekyll builds
-    .pipe(gulp.dest(config.dest));
+    return gulp.src(config.src)
+        .pipe($.sourcemaps.init())
+        .pipe($.babel())
+        .pipe($.concat(config.filename))
+        .pipe($.if(isProduction, $.uglify({mangle: false})))
+        .pipe($.if(!isProduction, $.sourcemaps.write()))
+        // for live injecting (for production builds we write the revised version)
+        .pipe($.if(!isProduction, gulp.dest(config.dev.dest)))
+        // for future jekyll builds
+        .pipe(gulp.dest(config.dest));
 });
